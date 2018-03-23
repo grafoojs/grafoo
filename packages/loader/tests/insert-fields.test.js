8abba10eabd0032c8f40251bb534f5c207031767
malbernaz <albernazmiguel@gmail.com>
2018-03-23 10:59:59 -0300

    build process for loader

--- packages/loader/tests/insert-fields.test.js
+++ packages/loader/tests/insert-fields.test.js
@@ -8,15 +8,15 @@ import insertFields from "../src/insert-fields";
 const schema = fs.readFileSync(path.join(__dirname, "..", "schema.graphql"), "utf-8");
 
 test("should insert a field", t => {
-  const query = `{ user { name } }`;
-  const expected = `{ user { name id } }`;
+  const query = "{ user { name } }";
+  const expected = "{ user { name id } }";
 
   t.is(print(insertFields(schema, parse(query), ["id"])), print(parse(expected)));
 });
 
 test("should insert more then a field if specified", t => {
-  const query = `{ user { name } }`;
-  const expected = `{ user { name username email id } }`;
+  const query = "{ user { name } }";
+  const expected = "{ user { name username email id } }";
 
   t.is(
     print(insertFields(schema, parse(query), ["username", "email", "id"])),
@@ -25,8 +25,8 @@ test("should insert more then a field if specified", t => {
 });
 
 test("should insert `__typename` if specified", t => {
-  const query = `{ user { name } } `;
-  const expected = `{ user { name __typename } }`;
+  const query = "{ user { name } }";
+  const expected = "{ user { name __typename } }";
 
   t.is(print(insertFields(schema, parse(query), ["__typename"])), print(parse(expected)));
 });
