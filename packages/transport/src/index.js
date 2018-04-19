eb4065e61a980995fae72cb75f7d8af90e844325
malbernaz <albernazmiguel@gmail.com>
2018-04-19 11:03:18 -0300

    updates

--- packages/transport/src/index.js
+++ packages/transport/src/index.js
@@ -8,13 +8,13 @@ export default function createTransport(uri, fetchOptions) {
   return request => {
     const init = fetchOptions.call ? fetchOptions() : fetchOptions;
 
-    const options = assign(init, {
+    assign(init, {
       body: JSON.stringify(request),
       method: "POST",
       headers: assign({}, init.headers, { "Content-Type": "application/json" })
     });
 
-    return fetch(uri, options)
+    return fetch(uri, init)
       .then(res => res.json())
       .then(({ data, errors }) => {
         if (errors) throw new GraphQlError(errors, request);
