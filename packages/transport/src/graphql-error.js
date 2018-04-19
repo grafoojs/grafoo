ee213a62f3a26f08a00cb7b3e69d83b4f0e72a8f
malbernaz <albernazmiguel@gmail.com>
2018-04-19 14:04:00 -0300

    trim transport

--- packages/transport/src/graphql-error.js
+++ packages/transport/src/graphql-error.js
@@ -1,14 +1,13 @@
+import { assign } from "@grafoo/util";
+
 const Err = Error;
 
 export default function GraphQlError(errors, request) {
   Err.call(this);
 
-  this.message = "graphql error on request " + JSON.stringify(request);
-  this.errors = errors;
+  assign(this, { message: "graphql error on request " + JSON.stringify(request), errors });
 
-  if (Err.captureStackTrace) {
-    Err.captureStackTrace(this, GraphQlError);
-  }
+  if (Err.captureStackTrace) Err.captureStackTrace(this, GraphQlError);
 }
 
 (GraphQlError.prototype = Object.create(Err.prototype)).constructor = GraphQlError;
