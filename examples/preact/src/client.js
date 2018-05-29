438278fbc3175c3ddb7a55b03173acb4c497b25f
malbernaz <albernazmiguel@gmail.com>
2018-05-29 01:32:28 -0300

    remove remaining idFromProps from the code base; update readme

--- examples/preact/src/client.js
+++ examples/preact/src/client.js
@@ -2,9 +2,7 @@ import createClient from "@grafoo/core";
 
 const API_URL = "https://api.graph.cool/simple/v1/cj28ccc28umr50115gjodwzix";
 
-const client = createClient(API_URL, {
-  idFromProps: _ => _.id
-});
+const client = createClient(API_URL, { idFromProps: ["id"] });
 
 if (process.env.NODE_ENV !== "production") window.client = client;
 
