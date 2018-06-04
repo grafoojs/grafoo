13e41f445bc86afee48778a77e81618f81c21294
malbernaz <albernazmiguel@gmail.com>
2018-06-03 23:32:13 -0300

    try to fix ci

--- packages/bundle/cli.js
+++ packages/bundle/cli.js
@@ -3,26 +3,24 @@
 /* eslint-disable no-console */
 
 const mri = require("mri");
-const { exec } = require("child_process");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 const build = require(".");
 
 const opts = mri(process.argv.slice(2));
 const CWD = process.cwd();
 
-const createDef = exec("tsc -p tsconfig.build.json");
+(async () => {
+  try {
+    await exec("tsc -p tsconfig.build.json");
 
-createDef.stdout.pipe(stdout => {
-  console.log(stdout);
-});
-createDef.stderr.pipe(stderr => {
-  console.error(stderr);
-});
-
-createDef.on("close", () => {
-  build(
-    Object.assign(opts, {
-      skipCompression: !!opts["skip-compression"],
-      rootPath: CWD
-    })
-  ).catch(console.error);
-});
+    await build(
+      Object.assign(opts, {
+        skipCompression: !!opts["skip-compression"],
+        rootPath: CWD
+      })
+    );
+  } catch (stderr) {
+    console.error(stderr);
+  }
+})();
