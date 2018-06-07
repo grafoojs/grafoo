a8f589a3abf47237e8cc164be3e5ed3934cae214
malbernaz <albernazmiguel@gmail.com>
2018-06-07 20:52:51 -0300

    remove examples folder

--- examples/preact/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import { h, render } from "preact";
-import App from "./App";
-
-const mnt = document.getElementById("mnt");
-
-export default function bootstrap(client) {
-  render(<App client={client} />, mnt, mnt.lastChild);
-}
--- examples/react/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./app";
-
-export default function bootstrap(client) {
-  render(<App client={client} />, document.getElementById("mnt"));
-}
