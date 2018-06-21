017c2ffb35a4716ae889d0770c2190216ca3baa7
malbernaz <albernazmiguel@gmail.com>
2018-06-21 15:25:00 -0300

    refactor tests

--- scripts/jest-setup.js
+++ scripts/jest-setup.js
@@ -2,15 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const { transform } = require("@babel/core");
 
-const config = { sourceMap: "inline", ast: false };
-
-try {
-  Object.assign(config, JSON.parse(fs.readFileSync(path.join(process.cwd(), ".babelrc"), "utf-8")));
-} catch (e) {
-  Object.assign(config, {
-    presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"]
-  });
-}
+const config = Object.assign(
+  { sourceMap: "inline", ast: false },
+  JSON.parse(fs.readFileSync(path.join(process.cwd(), ".babelrc"), "utf-8"))
+);
 
 module.exports.process = (src, path) =>
   transform(src, Object.assign({}, config, { filename: path }));
