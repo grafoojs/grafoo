506e31784186d2e834bcfc4860cc817ee0eb5a85
malbernaz <albernazmiguel@gmail.com>
2018-06-20 21:31:54 -0300

    fixed react bug

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -36,34 +36,31 @@ let ctx = createContext({});
 export let Provider: SFC<Context> = props =>
   createElement(ctx.Provider, { value: props.client }, props.children);
 
+class GrafooConsumer extends Component {
+  constructor(props) {
+    super(props);
+
+    let { getState, unbind, load } = createBindings(props.client, props, () => this.forceUpdate());
+
+    this.componentDidMount = () => {
+      if (props.skip || !props.query || getState().loaded) return;
+
+      load();
+    };
+
+    this.componentWillUnmount = () => {
+      unbind();
+    };
+
+    this.render = (): ReactNode => props.children(getState());
+  }
+}
+
 /**
  * T = Query
  * U = Mutations
  */
 export let Consumer: ConsumerType = <T, U>(consumerProps: GrafooReactConsumerProps<T, U>) =>
-  createElement(ctx.Consumer, null, clientInstance =>
-    createElement(
-      class GrafooConsumer extends Component {
-        constructor(props: GrafooReactConsumerProps<T, U>) {
-          super(props);
-
-          let { getState, unbind, load } = createBindings<T, U>(clientInstance, props, () =>
-            this.forceUpdate()
-          );
-
-          this.componentDidMount = () => {
-            if (props.skip || !props.query || getState().loaded) return;
-
-            load();
-          };
-
-          this.componentWillUnmount = () => {
-            unbind();
-          };
-
-          this.render = (): ReactNode => props.children(getState());
-        }
-      },
-      consumerProps
-    )
+  createElement(ctx.Consumer, null, client =>
+    createElement(GrafooConsumer, Object.assign({ client }, consumerProps))
   );
