e4835426f731fdb2b6bce839d308ffc03ebc6992
malbernaz <albernazmiguel@gmail.com>
2018-06-25 17:31:23 -0300

    updates

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -40,20 +40,20 @@ export default function createClient(uri: string, options?: ClientOptions): Clie
 
     let objects: ObjectsMap = {};
 
-    for (let path in paths) {
-      let { name, args } = paths[path];
+    for (let i in paths) {
+      let { name, args } = paths[i];
       let pathData = { [name]: data[name] };
       let pathObjects = mapObjects(pathData, idFields);
 
       Object.assign(objects, pathObjects);
 
-      pathsMap[getPathId(path, args, variables)] = {
+      pathsMap[getPathId(i, args, variables)] = {
         data: pathData,
         objects: Object.keys(pathObjects)
       };
     }
 
-    // assign new values to objects
+    // assign new values to objects in objectsMap
     for (let i in objects) {
       objectsMap[i] = objects[i] = Object.assign({}, objectsMap[i], objects[i]);
     }
@@ -72,9 +72,9 @@ export default function createClient(uri: string, options?: ClientOptions): Clie
     let data = {};
     let objects: ObjectsMap = {};
 
-    for (let path in paths) {
-      let { name, args } = paths[path];
-      let currentPath = pathsMap[getPathId(path, args, variables)];
+    for (let i in paths) {
+      let { name, args } = paths[i];
+      let currentPath = pathsMap[getPathId(i, args, variables)];
 
       if (currentPath) {
         data[name] = currentPath.data[name];
@@ -83,9 +83,9 @@ export default function createClient(uri: string, options?: ClientOptions): Clie
       }
     }
 
-    if (!Object.keys(data).length) return {};
-
-    return { data: buildQueryTree(data, objectsMap, idFields), objects };
+    return Object.keys(data).length
+      ? { data: buildQueryTree(data, objectsMap, idFields), objects }
+      : {};
   }
 
   function flush() {
