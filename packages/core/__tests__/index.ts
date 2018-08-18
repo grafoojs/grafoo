66154b28b7d35c0f8859159d446a3e6538e78159
miguel albernaz <albernazmiguel@gmail.com>
2018-08-18 15:03:25 -0300

    uncomment tests

--- packages/core/__tests__/index.ts
+++ packages/core/__tests__/index.ts
@@ -125,31 +125,31 @@ describe("@grafoo/core", () => {
     client = createClient(mockTrasport, { idFields: ["id"] });
   });
 
-  // it("should be instantiable", () => {
-  //   expect(() => createClient(mockTrasport, { idFields: ["id"] })).not.toThrow();
-  //   expect(typeof client.execute).toBe("function");
-  //   expect(typeof client.listen).toBe("function");
-  //   expect(typeof client.write).toBe("function");
-  //   expect(typeof client.read).toBe("function");
-  //   expect(typeof client.flush).toBe("function");
-  // });
+  it("should be instantiable", () => {
+    expect(() => createClient(mockTrasport, { idFields: ["id"] })).not.toThrow();
+    expect(typeof client.execute).toBe("function");
+    expect(typeof client.listen).toBe("function");
+    expect(typeof client.write).toBe("function");
+    expect(typeof client.read).toBe("function");
+    expect(typeof client.flush).toBe("function");
+  });
 
-  // it("should perform query requests", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+  it("should perform query requests", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
 
-  //   let { query, frags } = POST_WITH_FRAGMENT;
-  //   if (frags) for (const frag in frags) query += " " + frags[frag];
+    let { query, frags } = POST_WITH_FRAGMENT;
+    if (frags) for (const frag in frags) query += " " + frags[frag];
 
-  //   const data = await executeQuery({ query, variables });
+    const data = await executeQuery({ query, variables });
 
-  //   expect(data).toEqual(await client.execute(POST_WITH_FRAGMENT, variables));
-  // });
+    expect(data).toEqual(await client.execute(POST_WITH_FRAGMENT, variables));
+  });
 
-  // it("should perform query requests with fragments", async () => {
-  //   const data = await executeQuery({ query: SIMPLE_AUTHORS.query });
+  it("should perform query requests with fragments", async () => {
+    const data = await executeQuery({ query: SIMPLE_AUTHORS.query });
 
-  //   expect(data).toEqual(await client.execute(SIMPLE_AUTHORS));
-  // });
+    expect(data).toEqual(await client.execute(SIMPLE_AUTHORS));
+  });
 
   it("should write queries to the client", async () => {
     const data = await executeQuery<PostsAndAuthorsQuery>(POSTS_AND_AUTHORS);
@@ -176,169 +176,169 @@ describe("@grafoo/core", () => {
     expect(() => client.read(POSTS_AND_AUTHORS)).not.toThrow();
   });
 
-  // it("should read queries from the client", async () => {
-  //   const { data } = await executeQuery<AuthorsQuery>(AUTHORS);
+  it("should read queries from the client", async () => {
+    const { data } = await executeQuery<AuthorsQuery>(AUTHORS);
 
-  //   client.write(AUTHORS, data);
+    client.write(AUTHORS, data);
 
-  //   const result = client.read<AuthorsQuery>(AUTHORS);
+    const result = client.read<AuthorsQuery>(AUTHORS);
 
-  //   const { authors } = data;
+    const { authors } = data;
 
-  //   expect(authors).toEqual(result.data.authors);
-  //   expect(authors.every(author => Boolean(result.objects[author.id]))).toBe(true);
-  //   expect(
-  //     authors.every(author => author.posts.every(post => Boolean(result.objects[post.id])))
-  //   ).toBe(true);
-  // });
+    expect(authors).toEqual(result.data.authors);
+    expect(authors.every(author => Boolean(result.objects[author.id]))).toBe(true);
+    expect(
+      authors.every(author => author.posts.every(post => Boolean(result.objects[post.id])))
+    ).toBe(true);
+  });
 
-  // it("should handle queries with variables", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
-  //   const { data } = await executeQuery<PostQuery>({ query: POST.query, variables });
+  it("should handle queries with variables", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+    const { data } = await executeQuery<PostQuery>({ query: POST.query, variables });
 
-  //   client.write(POST, variables, data);
+    client.write(POST, variables, data);
 
-  //   expect(client.read(POST, { id: "123" })).toEqual({});
-  //   expect(client.read<PostQuery>(POST, variables).data.post.id).toBe(variables.id);
-  // });
+    expect(client.read(POST, { id: "123" })).toEqual({});
+    expect(client.read<PostQuery>(POST, variables).data.post.id).toBe(variables.id);
+  });
 
-  // it("should flag if a query result is partial", async () => {
-  //   const { data } = await executeQuery<PostsQuery>({ query: POSTS.query });
+  it("should flag if a query result is partial", async () => {
+    const { data } = await executeQuery<PostsQuery>({ query: POSTS.query });
 
-  //   client.write(POSTS, data);
+    client.write(POSTS, data);
 
-  //   expect(client.read<PostsAndAuthorsQuery>(POSTS_AND_AUTHORS).partial).toBe(true);
-  // });
+    expect(client.read<PostsAndAuthorsQuery>(POSTS_AND_AUTHORS).partial).toBe(true);
+  });
 
-  // it("should remove unused objects from objectsMap", async () => {
-  //   const { data } = await executeQuery<AuthorsQuery>(SIMPLE_AUTHORS);
+  it("should remove unused objects from objectsMap", async () => {
+    const { data } = await executeQuery<AuthorsQuery>(SIMPLE_AUTHORS);
 
-  //   client.write(SIMPLE_AUTHORS, data);
+    client.write(SIMPLE_AUTHORS, data);
 
-  //   const authorToBeRemoved: Author = data.authors[0];
+    const authorToBeRemoved: Author = data.authors[0];
 
-  //   const ids = Object.keys(client.flush().objectsMap);
+    const ids = Object.keys(client.flush().objectsMap);
 
-  //   expect(ids.some(id => id === authorToBeRemoved.id)).toBe(true);
+    expect(ids.some(id => id === authorToBeRemoved.id)).toBe(true);
 
-  //   client.write(SIMPLE_AUTHORS, {
-  //     authors: data.authors.filter(author => author.id !== authorToBeRemoved.id)
-  //   });
+    client.write(SIMPLE_AUTHORS, {
+      authors: data.authors.filter(author => author.id !== authorToBeRemoved.id)
+    });
 
-  //   const nextIds = Object.keys(client.flush().objectsMap);
+    const nextIds = Object.keys(client.flush().objectsMap);
 
-  //   expect(nextIds.length).toBe(ids.length - 1);
-  //   expect(nextIds.some(id => id === authorToBeRemoved.id)).toBe(false);
-  // });
+    expect(nextIds.length).toBe(ids.length - 1);
+    expect(nextIds.some(id => id === authorToBeRemoved.id)).toBe(false);
+  });
 
-  // it("should perform update to client", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
-  //   const { data } = await executeQuery<PostQuery>({ query: POST.query, variables });
+  it("should perform update to client", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+    const { data } = await executeQuery<PostQuery>({ query: POST.query, variables });
 
-  //   client.write(POST, variables, data);
+    client.write(POST, variables, data);
 
-  //   const {
-  //     data: { post }
-  //   } = client.read<PostQuery>(POST, variables);
+    const {
+      data: { post }
+    } = client.read<PostQuery>(POST, variables);
 
-  //   expect(post.title).toBe("Quam odit");
+    expect(post.title).toBe("Quam odit");
 
-  //   client.write(POST, variables, { post: { ...post, title: "updated title" } });
+    client.write(POST, variables, { post: { ...post, title: "updated title" } });
 
-  //   expect(client.read<PostQuery>(POST, variables).data.post.title).toBe("updated title");
-  // });
+    expect(client.read<PostQuery>(POST, variables).data.post.title).toBe("updated title");
+  });
 
-  // it("should reflect updates on queries with shared objects", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
-  //   const postData = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
-  //   const postsData = (await executeQuery<PostsQuery>({ query: POSTS.query, variables })).data;
+  it("should reflect updates on queries with shared objects", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+    const postData = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
+    const postsData = (await executeQuery<PostsQuery>({ query: POSTS.query, variables })).data;
 
-  //   client.write(POSTS, postsData);
+    client.write(POSTS, postsData);
 
-  //   const { posts } = client.read<PostsQuery>(POSTS).data;
+    const { posts } = client.read<PostsQuery>(POSTS).data;
 
-  //   expect(posts.find(p => p.id === variables.id).title).toBe("Quam odit");
+    expect(posts.find(p => p.id === variables.id).title).toBe("Quam odit");
 
-  //   client.write(POST, variables, { post: { ...postData.post, title: "updated title" } });
+    client.write(POST, variables, { post: { ...postData.post, title: "updated title" } });
 
-  //   const { posts: updatedPosts } = client.read<PostsQuery>(POSTS, variables).data;
+    const { posts: updatedPosts } = client.read<PostsQuery>(POSTS, variables).data;
 
-  //   expect(updatedPosts.find(p => p.id === variables.id).title).toBe("updated title");
-  // });
+    expect(updatedPosts.find(p => p.id === variables.id).title).toBe("updated title");
+  });
 
-  // it("should merge objects in the client when removing or adding properties", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
-  //   const data = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
+  it("should merge objects in the client when removing or adding properties", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+    const data = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
 
-  //   client.write(POST, variables, data);
+    client.write(POST, variables, data);
 
-  //   const post = JSON.parse(JSON.stringify(client.read<PostQuery>(POST, variables).data.post));
+    const post = JSON.parse(JSON.stringify(client.read<PostQuery>(POST, variables).data.post));
 
-  //   delete post.__typename;
+    delete post.__typename;
 
-  //   post.foo = "bar";
+    post.foo = "bar";
 
-  //   client.write(POST, variables, { post });
+    client.write(POST, variables, { post });
 
-  //   expect(client.read<PostQuery>(POST, variables).data.post).toEqual({
-  //     __typename: "Post",
-  //     author: {
-  //       __typename: "Author",
-  //       id: "a1d3a2bc-e503-4640-9178-23cbd36b542c",
-  //       name: "Murphy Abshire"
-  //     },
-  //     body: "Ducimus harum delectus consectetur.",
-  //     id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85",
-  //     title: "Quam odit",
-  //     foo: "bar"
-  //   });
-  // });
+    expect(client.read<PostQuery>(POST, variables).data.post).toEqual({
+      __typename: "Post",
+      author: {
+        __typename: "Author",
+        id: "a1d3a2bc-e503-4640-9178-23cbd36b542c",
+        name: "Murphy Abshire"
+      },
+      body: "Ducimus harum delectus consectetur.",
+      id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85",
+      title: "Quam odit",
+      foo: "bar"
+    });
+  });
 
-  // it("should call client listeners on write with paths objects as arguments", async () => {
-  //   const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
-  //   const data = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
+  it("should call client listeners on write with paths objects as arguments", async () => {
+    const variables = { id: "2c969ce7-02ae-42b1-a94d-7d0a38804c85" };
+    const data = (await executeQuery<PostQuery>({ query: POST.query, variables })).data;
 
-  //   const listener = jest.fn();
-  //   const listener2 = jest.fn();
+    const listener = jest.fn();
+    const listener2 = jest.fn();
 
-  //   const unlisten = client.listen(listener);
-  //   client.listen(listener2);
+    const unlisten = client.listen(listener);
+    client.listen(listener2);
 
-  //   client.write(POST, variables, data);
+    client.write(POST, variables, data);
 
-  //   expect(listener).toHaveBeenCalledWith(client.read(POST, variables).objects);
+    expect(listener).toHaveBeenCalledWith(client.read(POST, variables).objects);
 
-  //   unlisten();
-  //   client.write(POST, variables, data);
+    unlisten();
+    client.write(POST, variables, data);
 
-  //   expect(listener).toHaveBeenCalledTimes(1);
-  //   expect(listener2).toHaveBeenCalledTimes(2);
+    expect(listener).toHaveBeenCalledTimes(1);
+    expect(listener2).toHaveBeenCalledTimes(2);
 
-  //   unlisten();
-  //   client.write(POST, variables, data);
+    unlisten();
+    client.write(POST, variables, data);
 
-  //   expect(listener2).toHaveBeenCalledTimes(3);
-  // });
+    expect(listener2).toHaveBeenCalledTimes(3);
+  });
 
-  // it("should be able read from the client with a declared initialState", async () => {
-  //   const { data } = await executeQuery(POSTS_AND_AUTHORS);
+  it("should be able read from the client with a declared initialState", async () => {
+    const { data } = await executeQuery(POSTS_AND_AUTHORS);
 
-  //   client.write(POSTS_AND_AUTHORS, data);
+    client.write(POSTS_AND_AUTHORS, data);
 
-  //   client = createClient(mockTrasport, { idFields: ["id"], initialState: client.flush() });
+    client = createClient(mockTrasport, { idFields: ["id"], initialState: client.flush() });
 
-  //   expect(client.read(POSTS_AND_AUTHORS).data).toEqual(data);
-  // });
+    expect(client.read(POSTS_AND_AUTHORS).data).toEqual(data);
+  });
 
-  // it("should accept `idFields` array in options", async () => {
-  //   const { data } = await executeQuery(AUTHORS);
+  it("should accept `idFields` array in options", async () => {
+    const { data } = await executeQuery(AUTHORS);
 
-  //   const client = createClient(mockTrasport, { idFields: ["__typename", "id"] });
+    const client = createClient(mockTrasport, { idFields: ["__typename", "id"] });
 
-  //   client.write(AUTHORS, data);
+    client.write(AUTHORS, data);
 
-  //   const cachedIds = Object.keys(client.flush().objectsMap);
+    const cachedIds = Object.keys(client.flush().objectsMap);
 
-  //   expect(cachedIds.every(key => /(Post|Author)/.test(key))).toBe(true);
-  // });
+    expect(cachedIds.every(key => /(Post|Author)/.test(key))).toBe(true);
+  });
 });
